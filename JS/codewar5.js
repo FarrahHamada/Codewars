f86314324f985add95a86e0939cdce8284e5b3df
FarrahHamada <68291767+FarrahHamada@users.noreply.github.com>
2020-08-14 21:32:21 -0400

    Syntax Debug

---
+++ JS/codewar5.js
@@ -0,0 +1,14 @@
+
+// If/else syntax debug
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+
+
+function checkAlive (health) {
+  if (health <= 0) {
+    return false
+  } else {
+
+    return true
+  }
+}
